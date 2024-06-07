617e0bea91f402cc1eed524a8142d7979c9fcf9e
Ulmas Halikov <halikovulmas416@gmail.com>
2024-06-08 00:53:12 +0300

    add bottom navigation ba

---
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/appointment/AppointmentFragment.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication.ui.appointment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentAppointmentBinding
+
+class AppointmentFragment : Fragment() {
+    private var _binding: FragmentAppointmentBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAppointmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/medicCard/MedicCardFragment.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication.ui.medicCard
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.FragmentMedicCardBinding
+
+class MedicCardFragment: Fragment() {
+        private var _binding: FragmentMedicCardBinding? = null
+            private val binding get() = _binding!!
+            override fun onCreateView(
+                inflater: LayoutInflater,
+                container: ViewGroup?,
+                savedInstanceState: Bundle?
+            ): View {
+                _binding = FragmentMedicCardBinding.inflate(inflater, container, false)
+                return binding.root
+            }
+
+            override fun onDestroy() {
+                super.onDestroy()
+                _binding = null
+            }
+}
